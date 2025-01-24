0693d4cf3faaacd57854cc4715378b080e1e2ede
prabhakar <prabhakarpandey7061@gmail.com>
2025-01-24 20:08:10 +0530

    changed file name

---
+++ Typeofdata.java
@@ -0,0 +1,12 @@
+package exercise;
+
+public class Typeofdata {
+    // public static void main(String[]ars){
+    //     System.out.println("jay shri ram ");
+    // }
+    // primitive datatype
+    public static void  main(char []args){
+        int ver = 20;
+
+    }
+}
