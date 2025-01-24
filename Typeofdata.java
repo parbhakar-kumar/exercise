8a2be1e0f5d8fcc8d2403fcfd8c7e87484efd872
prabhakar <prabhakarpandey7061@gmail.com>
2025-01-24 20:57:24 +0530

    practice all datatype

--- Typeofdata.java
+++ Typeofdata.java
@@ -1,12 +1,29 @@
 package exercise;
 
 public class Typeofdata {
-    // public static void main(String[]ars){
-    //     System.out.println("jay shri ram ");
-    // }
-    // primitive datatype
-    public static void  main(char []args){
-        int ver = 20;
+    public static void main(String[]ars){
+        System.out.println("jay shri ram ");
+        int var = 20 ;
+        float var1= 34.56f;
+        double var2 = 356.904;
+        char var3 = 'r';
+        short var4= 20000;
+        long var5 = 100000;
+        boolean var6 = true;
+        
+
+        System.out.println(var);
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+        System.out.println(var6);                                                                        
+    
 
     }
+    
+    
+    // primitive datatype
+   
 }
