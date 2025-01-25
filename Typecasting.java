2272978c94f0400d39ccd6b3e101db44a48b2a39
prabhakar <prabhakarpandey7061@gmail.com>
2025-01-25 21:05:06 +0530

    solve some question of typecasting

--- Typecasting.java
+++ Typecasting.java
@@ -3,5 +3,12 @@ package exercise;
 public class Typecasting {
     public static void main (String[]args){
         System.out.println("type Casting");
+        //Automatic Casting -wide casting
+        float fvar = 23.494f;
+        double dvar = fvar;
+        System.out.println(dvar);
+        //manual casting- narrow casting
+        long lavar = (long) fvar;
+        System.out.println(lavar);
     }
 }
