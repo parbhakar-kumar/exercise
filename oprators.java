7e8106e5db87ce0037dd9f7d7ec102aeec3043fa
prabhakar <prabhakarpandey7061@gmail.com>
2025-01-26 14:52:00 +0530

    exerics of all oprators

---
+++ oprators.java
@@ -0,0 +1,22 @@
+package exercise;
+public class oprators{
+    public static void main(String[]args){
+        int a =34;
+        int b= 45;
+        System.out.println(a+b);
+        System.out.println(b-a);
+        System.out.println(b*a);
+        System.out.println(10%2);
+        System.out.println(10/2);
+        System.out.println(3<2);
+        boolean boi= 3!=2;
+        System.out.println(boi);
+        System.out.println(3==3);
+        System.out.println(5<=2);
+        //***************************** */
+        String bool = (1>2)? "Bara hai":"chota hai";
+        System.out.println( "bool=" +bool);
+
+    }
+
+}
