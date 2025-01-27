a3eb6480d6331999413e6d0674f3e9d17641ff70
prabhakar <prabhakarpandey7061@gmail.com>
2025-01-27 17:11:16 +0530

    practice of all input

--- inputoutput.java
+++ inputoutput.java
@@ -1,5 +1,21 @@
 package exercise;
 
+import java.util.Scanner;
+
 public class inputoutput {
-    
+    public static void main(String[] args) {
+        int a =2;
+        // System.out.println(a);
+        // System.out.println(args[0]);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter the user inpput");
+        a = scan.nextInt();
+        System.out.println("output ="+a);
+        System.out.println("enter the string");
+        scan.nextLine();
+        String str = scan.nextLine();
+        System.out.println("output="+str);
+
+
+    }
 }
