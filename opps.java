78b5430a6d81ce8a053b9c2e3dacd21e5c6a4976
prabhakar <prabhakarpandey7061@gmail.com>
2025-03-10 23:06:44 +0530

    oops

--- opps.java
+++ opps.java
@@ -1,5 +1,16 @@
 package exercise;
-
+class employ{
+    int empId= 2;
+    int basicSal=20000;
+    int incetive =200;
+    void countSaleries(){
+        System.out.println(basicSal+incetive);
+    }
+}
 public class opps {
-    
+    public static void main(String[]args){
+        employ engineer = new employ();
+        System.out.println(employ.empId);
+
+    }
 }
