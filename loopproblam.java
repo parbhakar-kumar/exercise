4e53a0a7cae3a6aebecde1f1238f63cb8931ec62
prabhakar <prabhakarpandey7061@gmail.com>
2025-03-17 13:58:22 +0530

    pattern 3

--- loopproblam.java
+++ loopproblam.java
@@ -111,20 +111,32 @@ public class loopproblam {
    
 // }
 
-public static void main (String[]arge){
+// public static void main (String[]arge){
+//     Scanner scanner = new Scanner(System.in);
+//     System.out.print("Enter size");
+//     int n= scanner.nextInt();
+//     for(int row =1; row<=n;row++){
+//         for(int col=1;col<=n;col++)
+//         // System.out.print(col);
+//         System.out.print(row);
+//         System.out.println();
+
+//     }
+// }
+
+public static void main(String[]args){
+    char ch = 'A';
+
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter size");
-    int n= scanner.nextInt();
-    for(int row =1; row<=n;row++){
+    System.out.print("enter size");
+    int n = scanner.nextInt();
+    for (int row=1;row<=n;row++) {
         for(int col=1;col<=n;col++)
-        // System.out.print(col);
-        System.out.print(row);
+        System.out.print(ch++);
         System.out.println();
 
     }
+    
 }
 
-
 }
-
-
