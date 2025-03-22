e3c7f84870ba868a9932ed98865261487cc25969
prabhakar <prabhakarpandey7061@gmail.com>
2025-03-22 22:51:59 +0530

    fix

--- loopproblam.java
+++ loopproblam.java
@@ -140,18 +140,17 @@ public class loopproblam {
     
 // }
 
-public  static void main(String[]args){
-    Scanner scanner=new Scanner(System.in);
-    System.out.print("Enter size");
-    int n= scanner.nextInt();
-    for(int row=1;row<=n;row++){
-        char ch='A';
-        for(int col=1;col<=n;col++){
-            System.out.print(ch);
-            ch=(char)(ch+1);
-        }
-        System.out.println();
-    }
-}
-
-}
+// public  static void main(String[]args){
+//     Scanner scanner=new Scanner(System.in);
+//     System.out.print("Enter size");
+//     int n= scanner.nextInt();
+//     for(int row=1;row<=n;row++){
+//         char ch='A';
+//         for(int col=1;col<=n;col++){
+//             System.out.print(ch);
+//             ch=(char)(ch+1);
+//         }
+//         System.out.println();
+//     }
+// }
+// }
