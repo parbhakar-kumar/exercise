52fcc06486a2310154247dbf850a1ba2643d93ea
prabhakar <prabhakarpandey7061@gmail.com>
2025-03-22 23:36:00 +0530

    slove que

--- DadaG.java
+++ DadaG.java
@@ -1,5 +1,27 @@
 package exercise;
+//single lavle indarit
+
 
 public class DadaG {
-    
+    String name="om pandey" ;
+    String surname = "pandey";
+}
+class Father extends DadaG{
+    String name ="vinod";
+    int prp=50;
+
+}
+class Me extends Father{
+    String name = "Shivam";
+}
+class Mother{
+    String name="Mom";
+    int prp =20;
+}
+class run{
+    public static void main(String[]args){
+        Me m = new Me();
+        System.out.println(m.name);
+        System.out.println(m.surname);
+    }
 }
